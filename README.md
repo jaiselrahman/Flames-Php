70a49db851651b41ce16169caa7e1e162e73b775
JaiselRahman <jaisel20@gmail.com>
2017-10-09 18:29:33 +0530

    Added LICENSE

---
+++ README.md
@@ -0,0 +1,22 @@
+FLAMES
+======
+
+Find out FLAMES between two names:
+- **F**riend
+- **L**ove
+- **A**ffection
+- **M**arriage
+- **E**nemy
+- **S**ibling
+
+Visit http://jaiselrahman.ezyro.com/flames/ 
+(or)
+http://jaiselrahman.ezyro.com/flames/YOURNAME/ANOTHERNAME
+*Replace __YOURNAME__ with yourname and __ANOTHERNAME__  with the one you want to check it out.*
+
+Uses PHP, Slim for REST api in BackEnd and HTML with BootStrap in FrontEnd
+
+Source code is released under Apache License v2.0.
+Files under vendor/ [For Slim] and bs/ [Bootstrap] are release under its respective LICENSES.
+
+Copyright [2017] jaiselrahman
--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
+/**
+* Copyright [2017] [jaiselrahman]
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 ini_set('default_socket_timeout', 600);
 ini_set('mysql.connect_timeout', 600);
 
@@ -45,7 +61,7 @@ function printResponse($name1, $name2, $result) {
       <body class="container">
         <h1 class="bg-primary" style="text-align: center">Flames</h1>
         <div class="well" style="margin-top: 100px;">
-          <form class="form-horizontal" style="margin-top: 36px;" role="form" action="./index.php"
+          <form class="form-horizontal" style="margin-top: 36px;" role="form" action="/flames/index.php"
             method="GET">
             <div class="form-group row">
               <div class="col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6"> <input
